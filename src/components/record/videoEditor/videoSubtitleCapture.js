c3be10ede72c653d0186421e5ef041f8234ce2ed
Harrys Ravalomanana <hra@esokia-webagency.com>
2020-06-15 22:04:20 +0400

    PHRAS-3116 populate language field

--- src/components/record/videoEditor/videoSubtitleCapture.js
+++ src/components/record/videoEditor/videoSubtitleCapture.js
@@ -7,6 +7,30 @@ const videoSubtitleCapture = (services, datas, activeTab = false) => {
     const initialize = (params) => {
         let {$container, data} = params;
 
+
+        //Populate Language Select field by select id
+        function builCaptionlanguage(selectField) {
+            $.ajax({
+                type: 'GET',
+                url: url + 'prod/tools/videoTextTrack/field',
+                //url: 'https://next.json-generator.com/api/json/get/Ny78r9gpd',
+                success: function success(datas) {
+
+                    if (datas.length > 0) {
+
+                        var _html = '';
+                        datas.forEach(function (elem) {
+                            _html += '<option value=' + elem.meta_struct_id + '>' + elem.label + '</option>';
+                            console.log(elem);
+                        });
+                        $(selectField).append(_html);
+                    }
+                }
+            });
+        }
+
+        builCaptionlanguage('#subtitle-language-select');
+
         $container.on('click', '.add-subtitle-vtt', function (e) {
             e.preventDefault();
             addSubTitleVtt();
@@ -21,22 +45,56 @@ const videoSubtitleCapture = (services, datas, activeTab = false) => {
         // Set height of left block
         leftHeight = $('.video-subtitle-left-inner').closest('#tool-tabs').height();
         $('.video-subtitle-left-inner').css('height', leftHeight - 230);
-        $('.video-request-left-inner').css('height', leftHeight - 230);
+        $('.video-request-left-inner').css('height', leftHeight);
+
+
+        $('.endTime').on('keyup change', function (e) {
+            setDefaultStartTime();
+            endVal = stringToseconde($(this).val());
+            startVal = stringToseconde($(this).closest('.video-subtitle-item').find('.startTime').val());
+            diffVal = millisecondeToTime(endVal - startVal);
+            $(this).closest('.video-subtitle-item').find('.showForTime').val(diffVal);
+            setDefaultStartTime();
+        });
+        $('.startTime').on('keyup change', function (e) {
+            setDefaultStartTime();
+            startVal = stringToseconde($(this).val());
+            endVal = stringToseconde($(this).closest('.video-subtitle-item').find('.endTime').val());
+            diffVal = millisecondeToTime(endVal - startVal);
+            $(this).closest('.video-subtitle-item').find('.showForTime').val(diffVal);
+            setDefaultStartTime();
+        });
+        function setDefaultStartTime(e) {
+
+
+            var DefaultStartT = $('.video-subtitle-item:last .endTime').val();
+            DefaultStartT = stringToseconde(DefaultStartT) + 1;
+            DefaultStartT = millisecondeToTime(DefaultStartT);
+
+            var DefaultEndT = stringToseconde(DefaultStartT) + 2000;
+            DefaultEndT = millisecondeToTime(DefaultEndT);
+
+            $('#defaultStartValue').val(DefaultStartT);
+            $('#defaultEndValue').val(DefaultEndT);
+
+        }
 
         function setDiffTime(e) {
             $('.endTime').on('keyup change', function (e) {
-                setDefaultStartTime ();
+                setDefaultStartTime();
+                endVal = stringToseconde($(this).val());
+                startVal = stringToseconde($(this).closest('.video-subtitle-item').find('.startTime').val());
+                diffVal = millisecondeToTime(endVal - startVal);
+                $(this).closest('.video-subtitle-item').find('.showForTime').val(diffVal);
             });
             $('.startTime').on('keyup change', function (e) {
+                setDefaultStartTime();
                 startVal = stringToseconde($(this).val());
-                $(this).closest('.video-subtitle-item').find('.endTime').on('keyup change', function (e) {
-                    endVal = stringToseconde($(this).val());
-                    diffVal = millisecondeToTime(endVal - startVal);
-                    $(this).closest('.video-subtitle-item').find('.showForTime').val(diffVal);
-                });
-                setDefaultStartTime ();
+                endVal = stringToseconde($(this).closest('.video-subtitle-item').find('.endTime').val());
+                diffVal = millisecondeToTime(endVal - startVal);
+                $(this).closest('.video-subtitle-item').find('.showForTime').val(diffVal);
+
             });
-            return;
         }
 
         function stringToseconde(time) {
@@ -65,14 +123,6 @@ const videoSubtitleCapture = (services, datas, activeTab = false) => {
             $(this).closest('.video-subtitle-item').remove();
         });
 
-        function setDefaultStartTime (e) {
-            $('#defaultStartValue').val($('.video-subtitle-item:last .endTime').val());
-            var DefaultStartT = $('.video-subtitle-item:last .endTime').val();
-            var DefaultEndT = stringToseconde(DefaultStartT) + 2000;
-            DefaultEndT = millisecondeToTime(DefaultEndT);
-            $('#defaultEndValue').val(DefaultEndT);
-        }
-
         $('#submit-subtitle').on('click', function (e) {
             e.preventDefault();
             try {
@@ -86,7 +136,7 @@ const videoSubtitleCapture = (services, datas, activeTab = false) => {
                     while (i <= countSubtitle * 3) {
                         captionText += allData[i].value + " --> " + allData[i + 1].value + "\n" + allData[i + 2].value + "\n\n";
                         i = i + 3;
-                        if(i == (countSubtitle * 3) - 3 ) {
+                        if (i == (countSubtitle * 3) - 3) {
                             $('#record-vtt').val(captionText);
                             console.log(captionText);
 
@@ -97,7 +147,8 @@ const videoSubtitleCapture = (services, datas, activeTab = false) => {
                         }
 
 
-                    };
+                    }
+                    ;
                 }
 
             } catch (err) {
@@ -125,14 +176,14 @@ const videoSubtitleCapture = (services, datas, activeTab = false) => {
 
         const addSubTitleVtt = () => {
             let countSubtitle = $('.video-subtitle-item').length;
-            setDefaultStartTime ();
+            setDefaultStartTime();
             let item = $('#default-item').html();
             $('.fields-wrapper').append(item);
             $('.video-subtitle-item:last .time').attr('pattern', '[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9]{3}$');
             $('.video-subtitle-item:last .startTime').attr('name', 'startTime' + countSubtitle).addClass('startTime' + countSubtitle).val($('#defaultStartValue').val());
             $('.video-subtitle-item:last .endTime').attr('name', 'endTime' + countSubtitle).addClass('endTime' + countSubtitle).val($('#defaultEndValue').val());
             $('.video-subtitle-item:last .number').html(countSubtitle);
-            setDiffTime();
+            //setDiffTime();
         };
 
         //Subtitle Request Tab
@@ -141,7 +192,7 @@ const videoSubtitleCapture = (services, datas, activeTab = false) => {
             try {
                 var requestData = $('#video-subtitle-request').serializeArray();
                 requestData = JSON.parse(JSON.stringify(requestData));
-console.log(requestData)
+                console.log(requestData)
 
             } catch (err) {
                 return;
